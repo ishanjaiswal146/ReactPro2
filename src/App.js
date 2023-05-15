82721514feff90903e0ec3f65bf534993faf13a7
Ishan <ishanjaiswal@gmail.com>
2023-05-15 17:49:08 +0530

    Feature-Footer started

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
+import Footer from './components/Footer';
 function App() {
   return (
     <Router>
-      <Header/>
-       <Routes>
-        <Route path='/' element={<Home />} />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
-    
   );
 }
 
 export default App;
+ 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,12 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
+import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import img1 from  '../assets/1.jpg';
-import img2 from  '../assets/2.jpg';
-import img3 from  '../assets/3.jpg';
-import img4 from  '../assets/4.jpg';
-import img5 from  '../assets/5.png';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import img1 from '../assets/1.jpg';
+import img2 from '../assets/2.jpg';
+import img3 from '../assets/3.jpg';
+import img4 from '../assets/4.jpg';
+import img5 from '../assets/5.png';
 
 const headingOptions = {
   pos: 'absolute',
@@ -18,60 +18,59 @@ const headingOptions = {
   size: '4xl',
 };
 
-
 const Home = () => {
-  return ( <Box>
-    <MyCarousel />
+  return (
+    <Box>
+      <MyCarousel />
 
-    <Container maxW={'container.xl'} minH={'100vh'} p="16">
-      <Heading
-        textTransform={'uppercase'}
-        py="2"
-        w={'fit-content'}
-        borderBottom={'2px solid'}
-        m="auto"
-      >
-        Services
-      </Heading>
-
-      <Stack
-        h="full"
-        p={'4'}
-        alignItems={'center'}
-        direction={['column', 'row']}
-      >
-        <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
-
-        <Text
-          letterSpacing={'widest'}
-          lineHeight={'190%'}
-          p={['4', '16']}
-          textAlign={'center'}
+      <Container maxW={'container.xl'} minH={'100vh'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          py="2"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+          m="auto"
         >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo
-          tempora deleniti quod, est eos veniam autem asperiores rem, nam
-          laudantium ut, similique ducimus amet sequi et voluptas iusto rerum
-          at aliquid numquam corporis ipsa reprehenderit? Porro iusto magnam,
-          sit vero deleniti, non nemo rem accusamus neque, quibusdam ab.
-          Libero, reprehenderit recusandae labore eaque, aspernatur aliquid
-          modi soluta ullam voluptatum unde repellat in molestias quod?
-          Doloribus ratione illum quisquam earum consequuntur nam voluptatem,
-          velit culpa maiores. Inventore esse illum excepturi dolores est
-          natus aliquid maiores sunt omnis deserunt accusantium amet enim eos
-          adipisci minima hic, nam provident quo aperiam quasi vel?
-          Dignissimos accusantium aliquid aut vel explicabo voluptatum
-          molestias fugit harum reprehenderit incidunt. Quis cumque explicabo
-          omnis officiis autem dolorum!
-        </Text>
-      </Stack>
-    </Container>
-</Box>
+          Services
+        </Heading>
+
+        <Stack
+          h="full"
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+
+          <Text
+            letterSpacing={'widest'}
+            lineHeight={'190%'}
+            p={['4', '16']}
+            textAlign={'center'}
+          >
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo iste
+            praesentium sed aliquid eaque earum cupiditate assumenda ducimus
+            modi quaerat fuga iusto laudantium ab reiciendis fugiat optio quam
+            dignissimos, maiores deleniti. Consectetur odio alias quis dicta
+            voluptas sequi cum, quam voluptatum quo eaque ratione?Lorem ipsum
+            dolor sit, amet consectetur adipisicing elit. Exercitationem
+            consectetur debitis illum, omnis excepturi recusandae rerum ratione
+            tempore ipsum aut! Aliquid eaque repellendus incidunt corrupti ad
+            facere ut. Autem architecto nostrum perferendis exercitationem alias
+            corporis suscipit adipisci, culpa esse voluptatibus, laborum
+            perspiciatis voluptatum quisquam.
+          </Text>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
 
 const MyCarousel = () => (
   <Carousel
     autoPlay
     infiniteLoop
-    interval={1000}
+    interval={2000}
     showStatus={false}
     showThumbs={false}
     showArrows={false}
@@ -103,8 +102,6 @@ const MyCarousel = () => (
       </Heading>
     </Box>
   </Carousel>
+);
 
-  );   
-};
-
-export default Home
+export default Home;
